b91880f6929fe2693ee95a4cab3392c10737a176
mukhoiran <mkhoir.anam@gmail.com>
2019-04-08 17:23:12 +0700

    avoid sql injection

--- functions/user.php
+++ functions/user.php
@@ -3,6 +3,11 @@
 function register_user($user,$pass){
   global $link;
 
+  //escape sql injection
+  $user = mysqli_real_escape_string($link, $user);
+  $pass = mysqli_real_escape_string($link, $pass);
+
+  $pass = password_hash($pass, PASSWORD_DEFAULT);
   $query = "INSERT INTO users (username,password) VALUES ('$user','$pass')";
 
   if(mysqli_query($link, $query)){
